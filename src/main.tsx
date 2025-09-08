3f9dac083ff8a010eeb54ac3bbae297745150d1a
Keiko Higuchi <47127802+kkoisland@users.noreply.github.com>
2025-09-08 13:32:33 -0700

    refactor: clean up App entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
-const root = createRoot(document.getElementById("root")!);
+const container = document.getElementById("root");
+if (!container) throw new Error("Root element not found");
+const root = createRoot(container);
 
 root.render(
   <StrictMode>
