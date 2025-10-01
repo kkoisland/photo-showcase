bac1e915471bf93c5029c24d685714a71d560291
Keiko Higuchi <47127802+kkoisland@users.noreply.github.com>
2025-10-01 15:07:02 -0700

    feat: add 6 albums story for AlbumGrid

--- src/AlbumGrid.stories.tsx
+++ src/AlbumGrid.stories.tsx
@@ -41,7 +41,6 @@ export const Default: Story = {
 		],
 	},
 };
-
 export const ThreeAlbums: Story = {
 	args: {
 		albums: [
@@ -70,6 +69,75 @@ export const ThreeAlbums: Story = {
 				id: "a3",
 				title: "Test Album3",
 				coverUrl: "https://picsum.photos/300/500",
+				createdAt: "",
+				startDate: "2025-09-01",
+				endDate: "2025-09-15",
+				photoIds: [],
+				count: 8,
+			},
+		],
+	},
+};
+
+export const SixAlbums: Story = {
+	args: {
+		albums: [
+			{
+				id: "a1",
+				title: "Test Album1",
+				coverUrl: "https://picsum.photos/300/200",
+				createdAt: "",
+				startDate: "2025-09-01",
+				endDate: "",
+				shared: true,
+				photoIds: [],
+				count: 42,
+			},
+			{
+				id: "a2",
+				title: "Test Album2",
+				coverUrl: "https://picsum.photos/300/400",
+				createdAt: "",
+				startDate: "",
+				endDate: "",
+				photoIds: [],
+				count: 321,
+			},
+			{
+				id: "a3",
+				title: "Test Album3",
+				coverUrl: "https://picsum.photos/300/500",
+				createdAt: "",
+				startDate: "2025-09-01",
+				endDate: "2025-09-15",
+				photoIds: [],
+				count: 8,
+			},
+			{
+				id: "a4",
+				title: "Test Album4",
+				coverUrl: "https://picsum.photos/300/600",
+				createdAt: "",
+				startDate: "",
+				endDate: "",
+				shared: true,
+				photoIds: [],
+				count: 420,
+			},
+			{
+				id: "a5",
+				title: "Test Album5",
+				coverUrl: "https://picsum.photos/300/700",
+				createdAt: "",
+				startDate: "",
+				endDate: "",
+				photoIds: [],
+				count: 1003,
+			},
+			{
+				id: "a6",
+				title: "Test Album6",
+				coverUrl: "https://picsum.photos/300/800",
 				createdAt: "",
 				startDate: "2025-09-01",
 				endDate: "2025-09-15",
